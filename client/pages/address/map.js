d73d424f3957415e1b886b019cd449b26561c69e
qcgm1978 <zhanghl@strongunion.com.cn>
2018-07-24 16:12:40 +0800

    marker label

--- client/pages/address/map.js
+++ client/pages/address/map.js
@@ -1,27 +1,51 @@
 export default {
-  onReady: function (e) {
+  onReady: function(e) {
     // 使用 wx.createMapContext 获取 map 上下文
     this.mapCtx = wx.createMapContext('map')
   },
-  getCenterLocation: function () {
+  getCenterLocation: function() {
     this.mapCtx.getCenterLocation({
-      success: function (res) {
+      success: function(res) {
         console.log(res.longitude)
         console.log(res.latitude)
       }
     })
   },
-  getPos(){
-    const that=this;
+  getPos() {
+    const that = this;
     wx.getLocation({
-      success(pos){
+      success(pos) {
         that.setData({
-          pos
+          pos,
+          markers: [{
+            id: 1,
+            latitude: pos.latitude-0.01,
+            longitude: pos.longitude-0.01,
+            // callout:{
+            //   content:'shop1'
+            // },
+            label: {
+              content: 'shop1',
+              textAlign: "center",
+              // anchorX: pos.longitude - 0.02,
+              // anchorY: 39.9279,
+              // padding:0.5
+            },
+            name:'shop1'
+          }, {
+            id: 2,
+              latitude: pos.latitude + 0.01,
+              longitude: pos.longitude + 0.01,
+            label: {
+              content: 'shop2'
+            },
+            name: 'shop2'
+          }]
         })
       }
     });
   },
-  bindMapTap(){
+  bindMapTap() {
     debugger;
   },
   regionchange(e) {
