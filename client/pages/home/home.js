7a11861701e13dc6dfc098ddc4efc91f7d4ff251
qcgm1978 <zhanghl@strongunion.com.cn>
2018-08-14 14:48:04 +0800

    make call

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -11,7 +11,11 @@ Page({
   data: {
     productList: [], // 商品列表
   },
-
+  callPhone(evt) {
+      wx.makePhoneCall({
+        phoneNumber: '400-101-5288' //仅为示例，并非真实的电话号码
+      })
+  },
   getProductList() {
     wx.showLoading({
       title: '商品数据加载中...',
@@ -94,7 +98,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.getProductList();
     // this.onLaunch()
 
@@ -103,49 +107,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
