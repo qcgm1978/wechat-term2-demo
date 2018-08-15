f476f0df81e536a4dfdd309103ce8ff874bea560
qcgm1978 <zhanghl@strongunion.com.cn>
2018-08-15 18:46:14 +0800

    profile page add

--- client/app.js
+++ client/app.js
@@ -31,7 +31,39 @@ App({
     this.login();
     // this.runWebSocket()  // 加载websocket操作    
   },
-
+  onShow(){
+    this.checkProgramUpdate();
+  },
+  checkProgramUpdate() {
+    if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        // console.log(res.hasUpdate);
+        if (res.hasUpdate) {
+          wx.showLoading({
+            title: '正在下载新版本'
+          })
+        }
+      })
+      updateManager.onUpdateReady(function () {
+        updateManager.applyUpdate()
+        // wx.showModal({
+        //   title: '更新提示',
+        //   content: '新版本已经准备好，是否重启应用？',
+        //   success: function (res) {
+        //     if (res.confirm) {
+        //       // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+        //       updateManager.applyUpdate()
+        //     }
+        //   }
+        // });
+      })
+      updateManager.onUpdateFailed(function () {
+        // 新的版本下载失败
+      });
+    }
+  },
   login({
     success,
     error
