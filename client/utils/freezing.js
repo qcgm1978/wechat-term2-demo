e28b64fa10db6867a073cc93de3f60ddaabfc851
qcgm1978 <zhanghl@strongunion.com.cn>
2018-12-07 09:49:29 +0800

    comment out test code

--- client/utils/freezing.js
+++ client/utils/freezing.js
@@ -2,7 +2,7 @@ var ERROR_CODE = require("index.js").config.errorCode;
 const FREEZING_TIME = ERROR_CODE.FREEZING_TIME
 const promptFreezing = () => {
   const isFreezing = isFreezingTime()
-    const currentPage = getCurrentPages().slice(-1)[0] 
+  const currentPage = getCurrentPages().slice(-1)[0]
   if (isFreezing && currentPage.data.isFreezing === undefined) {
     currentPage.setData({
       isFreezing: true
@@ -22,7 +22,7 @@ const verifyClientFreezing = () => {
 const isFreezingTime = () => {
   var a = new Date();
   // todo emulate 3 oclock
-  a.setHours(3)
+  // a.setHours(3)
   var hour = a.getHours();
   return hour < 4;
 }
@@ -44,11 +44,11 @@ const verifyFreezing = (statusCode) => {
   }
   return isFreezing
 }
-const disbaleOperation=()=>{
+const disbaleOperation = () => {
   const isFreezing = isFreezingTime()
   if (isFreezing) {
     verifyFreezing(FREEZING_TIME)
   }
   return isFreezing
 }
-export { verifyClientFreezing, promptFreezing, verifyFreezing, isFreezingTime, disbaleOperation}
+export { verifyClientFreezing, promptFreezing, verifyFreezing, isFreezingTime, disbaleOperation }
