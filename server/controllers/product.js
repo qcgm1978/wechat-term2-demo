4654c70f0be27ecedf0dfa2fae41ce0197766c92
qcgm1978 <zhanghl@strongunion.com.cn>
2018-12-07 10:58:06 +0800

    ditto

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -54,21 +54,68 @@ module.exports = {
 
   list: async ctx => {
     ctx.state = Mock.mock({
-      'result|20': [{
-        "itemId": "3467", "itemName": "冰露纯悦包装饮用水（纸箱）", "itemImageAddress1": "http://pro-img-jihuiduo.oss-cn-beijing.aliyuncs.com/sku_image/3467.jpg", "price": 19, "itemCategoryCode": "1204001", "putShelvesFlg": '@boolean', "itemSpecification": "550ml*24", "promotionTypes": null, "saleUnit": "箱(24个)",
-        notHasInventory: '@boolean'
-      }]
+      'result|20': [
+        {
+          "itemId": "3467",
+          "itemName": "冰露纯悦包装饮用水（纸箱）",
+          "itemImageAddress1": "http://pro-img-jihuiduo.oss-cn-beijing.aliyuncs.com/sku_image/3467.jpg",
+          "price": 19,
+          "itemCategoryCode": "1204001",
+          "putShelvesFlg": '@boolean',
+          "itemSpecification": "550ml*24",
+          "promotionTypes": null,
+          "saleUnit": "箱(24个)",
+          notHasInventory: '@boolean'
+        }
+      ]
     })
     // ctx.state.data = await DB.query("SELECT * FROM product;")
   },
-  hot: async ctx => {
-    ctx.state = Mock.mock({
-      'result|20': [{ "itemId": "3467", "itemName": "冰露纯悦包装饮用水（纸箱）", "itemImageAddress1": "http://pro-img-jihuiduo.oss-cn-beijing.aliyuncs.com/sku_image/3467.jpg", "price": 19, "itemCategoryCode": "1204001", "putShelvesFlg": true, "itemSpecification": "550ml*24", "promotionTypes": null, "saleUnit": "箱(24个)" }]
-    })
-  },
+
   detail: async ctx => {
     let productId = + ctx.params.orderId;
     let product
-    ctx.state = { "status": 200, "message": "OK", "result": [{ "itemId": "3467", "itemName": "冰露纯悦包装饮用水（纸箱）", "itemSpecification": "550ml*24", "itemBrand": "可口可乐", "saleUnit": "箱(24个)", "stockUnit": "个", "saleSku": "6928804010015", "stockSku": "6928804010015", "itemImageAddress1": "http://pro-img-jihuiduo.oss-cn-beijing.aliyuncs.com/sku_image/3467.jpg", "itemImageAddress2": "", "itemImageAddress3": "", "itemImageAddress4": "", "itemImageAddress5": "", "applayScope": "2", "salesDescription": "", "itemLocationCollection": "12,130,140,145,146,18,20,40,44,46,49,54,55,56,57,72,73,75,76,79,86,87,119,122,127,128,129,131,132,133,134,135,136,137,138,139,143,144", "itemCategoryCode": "1204001", "itemOrigin": "", "itemExpirationDays": "", "putShelvesDate": "2018/10/29 14:31:05", "price": 19, "itemCategoryName": " 可口可乐", "priceLevel": "47", "putShelvesFlg": true, "promotionTypes": null, "saleUnitExchange": 24, "purchaseType": "", "itemPackage": "瓶", "length": "", "width": "", "height": "", "weight": "", "isConsignedGood": "N" }] }
+    ctx.state = {
+      "status": 200,
+      "message": "OK",
+      "result": [
+        {
+          "itemId": "3467",
+          "itemName": "冰露纯悦包装饮用水（纸箱）",
+          "itemSpecification": "550ml*24",
+          "itemBrand": "可口可乐",
+          "saleUnit": "箱(24个)",
+          "stockUnit": "个",
+          "saleSku": "6928804010015",
+          "stockSku": "6928804010015",
+          "itemImageAddress1": "http://pro-img-jihuiduo.oss-cn-beijing.aliyuncs.com/sku_image/3467.jpg",
+          "itemImageAddress2": "",
+          "itemImageAddress3": "",
+          "itemImageAddress4": "",
+          "itemImageAddress5": "",
+          "applayScope": "2",
+          "salesDescription": "",
+          "itemLocationCollection": "12,130,140,145,146,18,20,40,44,46,49,54,55,56,57,72,73,75,76,79,86,87,119,122,127,128,129,131,132,133,134,135,136,137,138,139,143,144",
+          "itemCategoryCode": "1204001",
+          "itemOrigin": "",
+          "itemExpirationDays": "",
+          "putShelvesDate": "2018/10/29 14:31:05",
+          "price": 19,
+          "itemCategoryName": " 可口可乐",
+          "priceLevel": "47",
+          "putShelvesFlg": true,
+          notHasInventory: true,
+          "promotionTypes": null,
+          "saleUnitExchange": 24,
+          "purchaseType": "",
+          "itemPackage": "瓶",
+          "length": "",
+          "width": "",
+          "height": "",
+          "weight": "",
+          "isConsignedGood": "N"
+        }
+      ]
+    }
   }
 }
